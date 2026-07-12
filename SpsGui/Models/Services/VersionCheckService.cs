47e6b1cd91c905ee06149013e6d38009d36a1038
Pepton-0 <peptoncraft@gmail.com>
2026-07-12 22:38:59 +0900

    Revert version

--- SpsGui/Models/Services/VersionCheckService.cs
+++ SpsGui/Models/Services/VersionCheckService.cs
@@ -18,7 +18,7 @@ namespace SpsGui.Models.Services
     public class VersionCheckService : IVersionCheckService
     {
         private static readonly bool IgnoreLatestForDebug = false;
-        private static readonly string CurrentVersion = "1.1(Test)";
+        private static readonly string CurrentVersion = "1.1";
         private static readonly string repositoryName = "Pepton-0/SteamP2PScanner";
         private static JObject LatestRelease;
 
